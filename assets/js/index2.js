14d4ac16fa3a4203c6f8fd05dbbeaeb31b7df079
ValLR <valentinalopezrecabal@gmail.com>
2017-08-09 13:12:28 -0400

    commit

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -8,5 +8,18 @@ $(document).ready(function(){
 		}
 
 	});
+		$(document).ready(function() {
+	   var slider = document.getElementById('test-slider');
+	    $('.slider').slider();
+	    // Pause slider
+	    $('.slider').slider('pause');
+	    // Start slider
+	    $('.slider').slider('start');
+	    // Next slide
+	    $('.slider').slider('next');
+	    // Previous slide
+	    $('.slider').slider('prev');
+	 });
+
 	
 })
